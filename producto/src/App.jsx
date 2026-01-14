0d23dc14dba65af8a5be6b6e81bbff3962d16f2f
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2026-01-14 10:43:01 +0100

    Card Mall

--- producto/src/App.jsx
+++ producto/src/App.jsx
@@ -1,12 +1,19 @@
-
 import './App.css'
+import Card from './components/cards/Card'
 
 function App() {
 
   return (
-    <>
+    <div style={{ display: 'flex', gap: '2rem', flexWrap: 'wrap'}}>
+      <Card>
+        <h2>Aktivt arbetsblock</h2>
+        <p>25 minuter kvar</p>
+        <button>Starta</button>
+        <button>Pausa</button>
+      </Card>
 
-    </>
+    </div>
+
   )
 }
 
