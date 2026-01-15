bf1a474d85364066d91fad6aec78ccc4d0ef2603
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-01-15 14:16:42 +0100

    button component - template

--- producto/src/App.jsx
+++ producto/src/App.jsx
@@ -1,13 +1,32 @@
-
-import './App.css'
+import "./App.css";
+import Button from "./components/button";
 
 function App() {
-
   return (
-    <>
+    <div style={{ padding: "2rem" }}>
+      <h1>Button test</h1>
 
-    </>
-  )
+      <div style={{ display: "flex", gap: "1rem", marginTop: "2rem" }}>
+        <Button
+          text="Start"
+          variant="primary"
+          onClick={() => alert("Start button clicked!")}
+        />
+
+        <Button
+          text="Pause"
+          variant="secondary"
+          onClick={() => alert("Pause button clicked!")}
+        />
+
+        <Button
+          text="Stop"
+          variant="danger"
+          onClick={() => alert("Stop button clicked!")}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ producto/src/components/button.jsx
@@ -0,0 +1,15 @@
+import "./button.css";
+
+function Button({ text, onClick, variant = "primary", disabled = false }) {
+  return (
+    <button
+      className={`btn btn-${variant}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
