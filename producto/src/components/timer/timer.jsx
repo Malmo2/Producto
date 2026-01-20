6be762d3a3c5ea5f036ea68593916ad7001f5597
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-01-20 20:11:31 +0100

    formatTime

--- producto/src/components/timer/timer.jsx
+++ producto/src/components/timer/timer.jsx
@@ -8,6 +8,10 @@ function Timer () {
   const intervalRef = useRef(null);
 
   const formatTime = (seconds) => {
-    
-  }
+    const minutes = Math.floor(seconds / 60);
+    const seconds = seconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")};
+  };
+
+
 }
