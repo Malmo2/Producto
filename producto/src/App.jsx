796fa2378decab98846543352a0c7e2b915a123d
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-01-20 10:41:30 +0100

    navbar fixed + packages

--- producto/src/App.jsx
+++ producto/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Button from "./components/button/button";
 import Card from "./components/cards/Card";
-// import Upcoming from "./components/Upcoming/Upcoming";
 import Timer from "./components/timer/timer";
 
 import Upcoming from "./components/Upcoming/Upcoming";
@@ -10,67 +9,42 @@ import CalendarPopup from "./components/Calendar/Calendar";
 
 import Navbar from "./components/Nav/Navbar";
 import navLinks from "./components/Nav/navLinks";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
 
+function Insights() {
+  return <h1>Insights</h1>;
+}
 
+function Projects() {
+  return <h1>Projects</h1>;
+}
 
+function Schedule() {
+  return <h1>Schedule</h1>;
+}
 
+function Team() {
+  return <h1>Team</h1>;
+}
 
 function App() {
-
-
   return (
     <>
-
       <Navbar links={navLinks} />
 
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <PublicRoute>
-              <Dashboard />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/insights"
-          element={
-            <PublicRoute>
-              <Insights />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <PublicRoute>
-              <Projects />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/schedule"
-          element={
-            <PublicRoute>
-              <Schedule />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/team"
-          element={
-            <PublicRoute>
-              <Team />
-            </PublicRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
-
-
 
       <Card>
         <Button>Hejhej</Button>
@@ -81,15 +55,11 @@ function App() {
 
       <Timer />
 
-
       <aside className="aside-panel">
         <CalendarPopup />
         <Upcoming />
         <ActivityLog />
-
-
       </aside>
-
     </>
   );
 }
--- producto/src/components/nav/Navbar.jsx
+++ producto/src/components/nav/Navbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import './navbar.css';
 
 function Navbar({ links = [] }) {
     return (
