f3d28469161af15b991e5a54795126a90a6719dc
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-01-24 13:43:42 +0100

    re-added the styling to the right-side panel

--- producto/src/components/panels/Rightpanel.jsx
+++ producto/src/components/panels/Rightpanel.jsx
@@ -4,7 +4,7 @@ import styles from './rightpanel.module.css'
 export default function RightPanel({ children }) {
     return (
         <div className={styles.rightPanel}>
-            {children}
-        </div>
+            <div className={styles.panelBox}>{children}</div>
+        </div >
     );
 }
