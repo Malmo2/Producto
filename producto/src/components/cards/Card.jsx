d67a0308cd44328fa153f6e0383327c1897d7845
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-01-26 14:17:02 +0100

    fixing error
    d Untracked files:

--- producto/src/components/cards/Card.jsx
+++ producto/src/components/cards/Card.jsx
@@ -1,17 +1,17 @@
-
-function Card({ children, onClick }) {
-  return (
-    <div onClick={onClick} style={{ border: 'none', background: 'none', padding: 0 }}>
-      {children}
-    </div>
-
-function Card({ title, style, children, className = "", titleClassName = "", as: Component = "div", ...rest }) {
+function Card({
+  title,
+  style,
+  children,
+  className = "",
+  titleClassName = "",
+  as: Component = "div",
+  ...rest
+}) {
   return (
     <Component className={className} style={style} {...rest}>
       {title ? <h2 className={titleClassName}>{title}</h2> : null}
       <div>{children}</div>
     </Component>
-
   );
 }
 
