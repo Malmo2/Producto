fba2104c40b848807c788e91c2ceaf88b062b88c
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-03-02 15:38:57 +0100

    usenavigate on new sessions button in header

--- producto/src/components/panels/Header.jsx
+++ producto/src/components/panels/Header.jsx
@@ -1,12 +1,14 @@
 import { Button, Typography } from "../ui";
 import styles from "./Header.module.css";
 import Greeting from "../../utils/Greeting";
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
   const today = new Date();
   const weekDay = today.toLocaleDateString("en-US", { weekday: "long" });
   const month = today.toLocaleDateString("en-US", { month: "long" });
   const day = today.getDate();
+  const navigate = useNavigate();
 
   const name = "John Doe";
 
@@ -16,8 +18,10 @@ function Header() {
       <Typography variant="body1">
         Today is {weekDay}, {month} {day}
       </Typography>
-      <Button type="button" variant="primary">
-        New task
+      <Button
+        onClick={() => navigate('/timer')}
+        type="button" variant="primary">
+        New session
       </Button>
     </div>
   );
