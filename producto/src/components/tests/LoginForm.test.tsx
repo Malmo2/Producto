52dda40f155ea654d901524e80b50a6511716b54
meskilsson <mattiaseskilsson@hotmail.se>
2026-03-09 10:59:23 +0100

    added a fake module with mock so that testing will work

--- producto/src/components/tests/LoginForm.test.tsx
+++ producto/src/components/tests/LoginForm.test.tsx
@@ -2,6 +2,20 @@ import LoginForm from "../forms/LoginForm";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
+jest.mock("../../contexts/AuthContext", () => ({
+  useAuthState: () => ({
+    status: "anonymous",
+    error: null,
+    session: null,
+    user: null,
+  }),
+  useAuthActions: () => ({
+    signIn: jest.fn(),
+    signOut: jest.fn(),
+    clearError: jest.fn(),
+  }),
+}));
+
 test("renders the login form title", () => {
   render(
     <MemoryRouter>
@@ -9,6 +23,5 @@ test("renders the login form title", () => {
     </MemoryRouter>,
   );
 
-  const title = screen.getByRole("heading", { name: /login/i });
-  expect(title).toBeInTheDocument();
+  expect(screen.getByRole("heading", { name: /log in/i })).toBeInTheDocument();
 });
