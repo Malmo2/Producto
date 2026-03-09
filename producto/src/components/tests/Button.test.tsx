d4d28f5756a477dd54fb0664caed5ebbb9ac198c
meskilsson <mattiaseskilsson@hotmail.se>
2026-03-09 10:03:15 +0100

    (Button.test.tsx): Added an onclick userEvent test

--- producto/src/components/tests/Button.test.tsx
+++ producto/src/components/tests/Button.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from "../ui/Button";
+import userEvent from "@testing-library/user-event";
 
 test("Render a button with the correct text", () => {
   render(<Button>Save</Button>);
@@ -16,4 +17,13 @@ test("Button is disabled when disabled=true and is transfered as prop", () => {
   render(<Button disabled />);
   const button = screen.getByRole("button");
   expect(button).toBeDisabled();
+});
+
+test("Calls the onClick-callback when the user clicks on the button", async () => {
+  const user = userEvent.setup();
+  const onClicked = jest.fn();
+  render(<Button onClick={onClicked} />);
+  const button = screen.getByRole("button");
+  await user.click(button);
+  expect(onClicked).toHaveBeenCalledTimes(1);
 });
