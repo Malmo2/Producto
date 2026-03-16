8d04ba8e255abd4e977ed3b1dba3cbbd6e70692c
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-03-16 14:34:39 +0100

    slight fixes to index and tsconfig

--- producto-mobile/app/index.tsx
+++ producto-mobile/app/index.tsx
@@ -2,32 +2,35 @@ import { View, Text } from "react-native";
 import { useTimer } from "../features/timer/context/TimerContext";
 import { formatTime } from "@/features/timer/utils/formatTime";
 import BaseButton from "@/ui/Buttons/BaseButton";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function HomeScreen() {
   const { state, startTimer, pauseTimer, resetTimer } = useTimer();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-        gap: 12,
-      }}
-    >
-      <Text>Timer app is running</Text>
-      <Text>Mode: {state.mode}</Text>
-      <Text>Running: {state.isRunning ? "Yes" : "No"}</Text>
-      <Text>Custom Minutes: {state.customMinutes}</Text>
-      <Text>Time Left: {formatTime(state.timeLeft)}</Text>
+    <SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          gap: 12,
+        }}
+      >
+        <Text>Timer app is running</Text>
+        <Text>Mode: {state.mode}</Text>
+        <Text>Running: {state.isRunning ? "Yes" : "No"}</Text>
+        <Text>Custom Minutes: {state.customMinutes}</Text>
+        <Text>Time Left: {formatTime(state.timeLeft)}</Text>
 
-      <View style={{ width: "30%", gap: 10, marginTop: 12 }}>
-        <BaseButton title="Start" onPress={startTimer}>Start</BaseButton>
-        <BaseButton title="Pause" onPress={pauseTimer}>Pause</BaseButton>
-        <BaseButton title="Reset" onPress={resetTimer}>Reset</BaseButton>
+        <View style={{ width: "30%", gap: 10, marginTop: 12 }}>
+          <BaseButton onPress={startTimer}>Start</BaseButton>
+          <BaseButton onPress={pauseTimer}>Pause</BaseButton>
+          <BaseButton onPress={resetTimer}>Reset</BaseButton>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
